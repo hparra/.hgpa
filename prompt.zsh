c1e955f3cfefc86379ce14549c085a8760ef8f88
H Parra <h@hgpa.tv>
2026-03-07 01:10:00 -0800

    Optimize git prompt hooks and add refresh throttling

--- prompt.zsh
+++ prompt.zsh
@@ -4,32 +4,98 @@
 
 autoload -Uz vcs_info
 autoload -Uz add-zsh-hook
+zmodload zsh/datetime 2>/dev/null
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' stagedstr '%F{green}+%f'
-zstyle ':vcs_info:*' unstagedstr '%F{yellow}*%f'
+
+typeset -g HGPA_LAST_SHOW_DIRTY
+HGPA_LAST_SHOW_DIRTY=''
+typeset -g HGPA_LAST_SHOW_AHEADBEHIND
+HGPA_LAST_SHOW_AHEADBEHIND=''
+typeset -g HGPA_LAST_SHOW_WORKTREE
+HGPA_LAST_SHOW_WORKTREE=''
+typeset -g HGPA_VCS_CONFIG_CHANGED
+HGPA_VCS_CONFIG_CHANGED=0
+typeset -gi HGPA_LAST_VCS_REFRESH_MS
+HGPA_LAST_VCS_REFRESH_MS=0
+typeset -gA HGPA_AB_HEAD_BY_REPO
+typeset -gA HGPA_AB_UPSTREAM_BY_REPO
+typeset -gA HGPA_AB_MISC_BY_REPO
+
 zstyle ':vcs_info:git:*' formats       ' %F{cyan}%b%f%u%c%m'
 zstyle ':vcs_info:git:*' actionformats ' %F{cyan}%b%f%F{red}|%a%f%u%c%m'
 
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind git-worktree
+function hgpa-configure-vcs-info() {
+  local show_dirty="${HGPA_SHOW_DIRTY:-1}"
+  local show_aheadbehind="${HGPA_SHOW_AHEADBEHIND:-1}"
+  local show_worktree="${HGPA_SHOW_WORKTREE:-1}"
+  HGPA_VCS_CONFIG_CHANGED=0
+  if [[ "$show_dirty" == "$HGPA_LAST_SHOW_DIRTY" && "$show_aheadbehind" == "$HGPA_LAST_SHOW_AHEADBEHIND" && "$show_worktree" == "$HGPA_LAST_SHOW_WORKTREE" ]]; then
+    return 0
+  fi
+  HGPA_LAST_SHOW_DIRTY="$show_dirty"
+  HGPA_LAST_SHOW_AHEADBEHIND="$show_aheadbehind"
+  HGPA_LAST_SHOW_WORKTREE="$show_worktree"
+  HGPA_VCS_CONFIG_CHANGED=1
+
+  local -a hooks
+  hooks=()
+  [[ "$show_aheadbehind" == "1" ]] && hooks=(git-aheadbehind "${hooks[@]}")
+  [[ "$show_worktree" == "1" ]] && hooks=(git-worktree "${hooks[@]}")
+
+  if [[ "$show_dirty" == "1" ]]; then
+    zstyle ':vcs_info:*' check-for-changes true
+    zstyle ':vcs_info:*' stagedstr '%F{green}+%f'
+    zstyle ':vcs_info:*' unstagedstr '%F{yellow}*%f'
+    hooks=(git-untracked "${hooks[@]}")
+  else
+    zstyle ':vcs_info:*' check-for-changes false
+    zstyle ':vcs_info:*' stagedstr ''
+    zstyle ':vcs_info:*' unstagedstr ''
+  fi
+
+  zstyle ':vcs_info:git*+set-message:*' hooks "${hooks[@]}"
+}
 
 function +vi-git-untracked() {
-  if command git status --porcelain 2>/dev/null | command grep -q '^??'; then
+  [[ "${HGPA_SHOW_DIRTY:-1}" == "1" ]] || return 0
+  local has_untracked
+  has_untracked=$(
+    command git ls-files --others --exclude-standard --directory --no-empty-directory 2>/dev/null |
+      command head -n 1
+  ) || return 0
+  if [[ -n "$has_untracked" ]]; then
     hook_com[misc]+='%F{red}?%f'
   fi
 }
 
 function +vi-git-aheadbehind() {
+  [[ "${HGPA_SHOW_AHEADBEHIND:-1}" == "1" ]] || return 0
+  local repo_refs repo head upstream
+  repo_refs=$(command git rev-parse --show-toplevel --verify HEAD --verify @{upstream} 2>/dev/null) || return 0
+  repo=${repo_refs%%$'\n'*}
+  repo_refs=${repo_refs#*$'\n'}
+  head=${repo_refs%%$'\n'*}
+  upstream=${repo_refs#*$'\n'}
+
+  if [[ "${HGPA_AB_HEAD_BY_REPO[$repo]-}" == "$head" && "${HGPA_AB_UPSTREAM_BY_REPO[$repo]-}" == "$upstream" ]]; then
+    hook_com[misc]+="${HGPA_AB_MISC_BY_REPO[$repo]-}"
+    return 0
+  fi
+
   local ahead behind
-  ahead=$(command git rev-list --count @{upstream}..HEAD 2>/dev/null)
-  behind=$(command git rev-list --count HEAD..@{upstream} 2>/dev/null)
-  (( ahead )) && hook_com[misc]+=" %F{green}↑${ahead}%f"
-  (( behind )) && hook_com[misc]+=" %F{red}↓${behind}%f"
+  read -r behind ahead <<<"$(command git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null)" || return 0
+  local misc=''
+  (( ahead )) && misc+=" %F{green}↑${ahead}%f"
+  (( behind )) && misc+=" %F{red}↓${behind}%f"
+  HGPA_AB_HEAD_BY_REPO[$repo]="$head"
+  HGPA_AB_UPSTREAM_BY_REPO[$repo]="$upstream"
+  HGPA_AB_MISC_BY_REPO[$repo]="$misc"
+  hook_com[misc]+="$misc"
   return 0
 }
 
 function +vi-git-worktree() {
-  [[ "${HGPA_SHOW_WORKTREE:-0}" == "1" ]] || return 0
+  [[ "${HGPA_SHOW_WORKTREE:-1}" == "1" ]] || return 0
   local toplevel main_wt
   toplevel=$(command git rev-parse --show-toplevel 2>/dev/null) || return 0
   main_wt=$(command git worktree list 2>/dev/null | head -1 | awk '{print $1}')
@@ -37,6 +103,27 @@ function +vi-git-worktree() {
   hook_com[misc]+=" %F{magenta}⎇ ${main_wt:t}/${toplevel:t}%f"
 }
 
-add-zsh-hook precmd vcs_info
+function hgpa-vcs-precmd() {
+  if [[ "${HGPA_SHOW_GIT:-1}" != "1" ]]; then
+    vcs_info_msg_0_=''
+    return 0
+  fi
+  hgpa-configure-vcs-info
+  local -i refresh_ms="${HGPA_GIT_REFRESH_MS:-500}"
+  local -i now_ms=0
+  if (( refresh_ms > 0 )) && (( HGPA_VCS_CONFIG_CHANGED == 0 )); then
+    now_ms=$(( EPOCHREALTIME * 1000 ))
+    if (( now_ms - HGPA_LAST_VCS_REFRESH_MS < refresh_ms )); then
+      return 0
+    fi
+  fi
+  vcs_info
+  if (( refresh_ms > 0 )); then
+    (( now_ms == 0 )) && now_ms=$(( EPOCHREALTIME * 1000 ))
+    HGPA_LAST_VCS_REFRESH_MS=$now_ms
+  fi
+}
+
+add-zsh-hook precmd hgpa-vcs-precmd
 setopt PROMPT_SUBST
 PROMPT='%F{blue}%1~%f${vcs_info_msg_0_} %F{magenta}%#%f '
