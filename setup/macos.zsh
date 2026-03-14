1f5e5b9d86ffcbb871c81301a95828848c729b1f
H Parra <h@hgpa.tv>
2026-03-14 00:39:50 -0700

    Add Finder and Dock macOS defaults

--- setup/macos.zsh
+++ setup/macos.zsh
@@ -2,6 +2,35 @@
 
 set -e
 
-echo >&2 "finder hidden files: on"
+echo "macOS recommendations"
+
+echo 'finder: show all files'
 defaults write com.apple.finder AppleShowAllFiles -bool true
-killall Finder >/dev/null 2>&1 || true
+
+echo 'finder: show all filename extensions'
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo 'finder: show path bar'
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo 'finder: show status bar'
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo 'finder: keep folders first when sorting by name'
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+killall Finder
+
+echo 'dock: automatically hide and show'
+defaults write com.apple.dock autohide -bool true
+
+echo 'dock: do not show recent applications'
+defaults write com.apple.dock show-recents -bool false
+
+echo 'dock: minimize windows into application icon'
+defaults write com.apple.dock minimize-to-application -bool true
+
+echo 'dock: use scale effect when minimizing'
+defaults write com.apple.dock mineffect -string scale
+
+killall Dock
